80166c1738b6bd3136d82b1c477a8164652e1fe1
Andy_Dai <andy_dai@turn2cloud.com>
2025-08-29 15:39:56 +0800

    feat: 啟動不閃退

--- app/src/main/java/tw/com/andyawd/fastbookkeeping/MainActivity.kt
+++ app/src/main/java/tw/com/andyawd/fastbookkeeping/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import tw.com.andyawd.fastbookkeeping.ui.FastBookkeepingApp
 import tw.com.andyawd.fastbookkeeping.ui.theme.FastBookkeepingTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
